fda6c55c7dd29e3ff57ecefae1ce72418544989f
Xiaoyu <xyll313@gmail.com>
2017-06-29 11:09:58 +0300

    2darray

--- exercises/programming/c/datastructures-functions-heat/2d_array.c
+++ exercises/programming/c/datastructures-functions-heat/2d_array.c
@@ -6,19 +6,41 @@
 
 int main(void)
 {
-    int i, j;
+int i, j;
+double  array[NX][NY];    
     // Add here the definition for array with name 'array'
     // of size NX times NY (258x258)
-#error Define the array
 
-    // Initialize first the zeros
-#error Initialize the zeros for interior
+for(i=0;i<NX;i++){
+	for(j=1;j<NY;j++){
+ 		array[i][j]=0.0;
+         }
+                 }
+// Initialize first the zeros
 
-    // Initial conditions for left and right
-#error add boundary conditions for left and right
+for(j=0;j<NY;j++){
+	array[0][j]=20.0;
+	array[NX-1][j]=70.0;         
+         } 
+// Initial conditions for left and right
 
+
+for(i=0;i<NX;i++){
+	array[i][0]=5.0;
+ 	array[i][NY-1]=85.0;
+                 }
     // and top and bottom boundaries
-#error add boundary conditions for top and bottom
 
-    return 0;
+
+/*for(i=0;i<NX;i++){
+        for(j=0;j<NY;j++){
+        	printf("%f ", array[i][j]);  
+         }
+	printf("\n");
+                 }  */
+ 
+
+
+
+   return 0;
 }
